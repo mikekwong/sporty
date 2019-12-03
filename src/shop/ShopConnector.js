2ea1355802faf1c2a8ea9e26dab0cdba25bfda60
Mike Wong <wongmdesign@gmail.com>
2019-12-03 12:49:11 -0500

    connected shop to react-redux

---
+++ src/shop/ShopConnector.js
@@ -0,0 +1,47 @@
+import React, { Component } from "react";
+import { Switch, Route, Redirect } from "react-router-dom";
+import { connect } from "react-redux";
+import { loadData } from "../data/ActionCreators";
+import { DataTypes } from "../data/Types";
+import { Shop } from "./Shop";
+
+const filterProducts = (products = [], category) =>
+  !category || category === "All"
+    ? products
+    : products.filter(p => p.category.toLowerCase() === category.toLowerCase());
+
+class ShopConnector extends Component {
+  componentDidMount() {
+    this.props.loadData(DataTypes.CATEGORIES);
+    this.props.loadData(DataTypes.PRODUCTS);
+  }
+
+  render() {
+    return (
+      <Switch>
+        <Route
+          path="/shop/products/:category?"
+          render={routeProps => (
+            <Shop
+              {...this.props}
+              {...routeProps}
+              products={filterProducts(
+                this.props.products,
+                routeProps.match.params.category
+              )}
+            />
+          )}
+        />
+        <Redirect to="/shop/products" />
+      </Switch>
+    );
+  }
+}
+
+const mapStateToProps = dataStore => ({ ...dataStore });
+
+const mapDispatchToProps = {
+  loadData,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(ShopConnector);
