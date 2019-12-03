0f56ec74076d059471d3c3fc76ccc75c3b0dd2b7
Mike Wong <wongmdesign@gmail.com>
2019-12-03 12:07:41 -0500

    added initial components for shop

---
+++ src/ProductList.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import { directive } from "@babel/types";
+
+export default class ProductList extends Component {
+  render() {
+    if (this.props.products === null || this.props.products.length === 0) {
+      return <h5 className="p-2">No Products</h5>;
+    }
+    return this.props.products.map(p => (
+      <div className="directive card m-1 p-1 bg-light" key={p.id}>
+        <h4>
+          <span className="badge badge-pill badge-primary float-right">
+            ${p.price.toFixed(2)}
+          </span>
+        </h4>
+        <div className="card-text bg-white p-1">{p.description}</div>
+      </div>
+    ));
+  }
+}
