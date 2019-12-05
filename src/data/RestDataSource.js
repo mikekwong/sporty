9abd49469c46a690ad5b2edb32923108000b5b38
Mike Wong <wongmdesign@gmail.com>
2019-12-05 17:24:29 -0500

    added a few comments about axios

--- src/data/RestDataSource.js
+++ src/data/RestDataSource.js
@@ -3,6 +3,9 @@ import Axios from "axios";
 
 // GetData method uses Axios to send an HTTP request to the web service to get all of the available objects for a specified data type. The result from the GetData method is a Promise that is resolved when the response is received from the web service.
 
+// Config module for axios request options
+// https://kapeli.com/cheat_sheets/Axios.docset/Contents/Resources/Documents/index
+// http://www.programmersought.com/article/2666649937/
 export class RestDataSource {
   // Request parameters will be used to request pages and specify a category.
   // With 'params' as a parameter for axios, we can specify the query string for other filtering options
