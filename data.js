a4af32efb3b65a8fb6bdbc7d03d049ab1aa80739
Mike Wong <wongmdesign@gmail.com>
2019-12-06 14:19:34 -0500

    graphql schema resolvers added

--- data.js
+++ data.js
@@ -1,11 +1,10 @@
-let faker = require("faker");
-let data = [];
-let categories = ["Watersports", "Soccer", "Chess", "Running"];
+const faker = require("faker");
 faker.seed(100);
-
+let categories = ["Watersports", "Soccer", "Chess"];
+let products = [];
 for (let i = 1; i <= 503; i++) {
   let category = faker.helpers.randomize(categories);
-  data.push({
+  products.push({
     id: i,
     name: faker.commerce.productName(),
     category: category,
@@ -14,13 +13,48 @@ for (let i = 1; i <= 503; i++) {
   });
 }
 
-module.exports = () => {
-  return {
-    categories: categories,
-    products: data,
-    orders: [],
+// generate a number of fake orders so there is data to work with
+let orders = [];
+for (let i = 1; i <= 103; i++) {
+  let fname = faker.name.firstName();
+  let sname = faker.name.lastName();
+  let order = {
+    id: i,
+    name: `${fname} ${sname}`,
+    email: faker.internet.email(fname, sname),
+    address: faker.address.streetAddress(),
+    city: faker.address.city(),
+    zip: faker.address.zipCode(),
+    country: faker.address.country(),
+    shipped: faker.random.boolean(),
+    products: [],
   };
-};
+  let productCount = faker.random.number({ min: 1, max: 5 });
+  let product_ids = [];
+  while (product_ids.length < productCount) {
+    let candidateId = faker.random.number({ min: 1, max: products.length });
+    if (product_ids.indexOf(candidateId) === -1) {
+      product_ids.push(candidateId);
+    }
+  }
+  for (let j = 0; j < productCount; j++) {
+    order.products.push({
+      quantity: faker.random.number({ min: 1, max: 10 }),
+      product_id: product_ids[j],
+    });
+  }
+  orders.push(order);
+}
+
+module.exports = () => ({ categories, products, orders });
+
+// module.exports = () => {
+//   return {
+//     categories: categories,
+//     products: data,
+//     orders: [],
+//   };
+// };
 
 // module.exports = function () {
 //   return {
