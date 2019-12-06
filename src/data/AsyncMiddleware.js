b23ba55e61492958e65312dc794a389f0f2f4999
Mike Wong <wongmdesign@gmail.com>
2019-12-06 12:38:24 -0500

    added form, refactored routes

--- src/data/AsyncMiddleware.js
+++ src/data/AsyncMiddleware.js
@@ -2,7 +2,7 @@
 
 // function that checks to see whether an action’s payload is a Promise, which it does by looking for function or objects that have a then function.
 const isPromise = payload =>
-  (typeof payload === "object" || typeof (payload === "function")) &&
+  (typeof payload === "object" || typeof payload === "function") &&
   typeof payload.then === "function";
 
 // The asyncAction function will be used as the data store middleware, and it calls then on the Promise to wait for it to be resolved, at which point it uses the result to replace the payload and passes it on, using the next function, which continues the normal path through the data store
--- src/shop/ShopConnector.js
+++ src/shop/ShopConnector.js
@@ -1,17 +1,14 @@
 import React, { Component } from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
-import { loadData } from "../data/ActionCreators";
+import * as ShopActions from "../data/ActionCreators";
 import { DataTypes } from "../data/Types";
 import Shop from "./Shop";
-import {
-  addToCart,
-  updateCartQuantity,
-  removeFromCart,
-  clearCart,
-} from "../data/CartActionCreators";
+import * as CartActions from "../data/CartActionCreators";
 import CartDetails from "./CartDetails";
 import DataGetter from "../data/DataGetter";
+import Checkout from "./Checkout";
+import Thanks from "./Thanks";
 
 // const filterProducts = (products = [], category) =>
 //   !category || category === "All"
@@ -24,6 +21,30 @@ class ShopConnector extends Component {
     // this.props.loadData(DataTypes.PRODUCTS);
   }
 
+  selectComponent = routeProps => {
+    const wrap = (Component, Content) => (
+      <Component {...this.props} {...routeProps}>
+        {Content && wrap(Content)}
+      </Component>
+    );
+    switch (routeProps.match.params.section) {
+      case "products":
+        return wrap(DataGetter, Shop);
+        break;
+      case "cart":
+        return wrap(CartDetails);
+        break;
+      case "checkout":
+        return wrap(Checkout);
+        break;
+      case "thanks":
+        return wrap(Thanks);
+      default:
+        /* Redirect matches any other URLs and redirects them to the URL for the first page of the products, unfiltered by category. */
+        return <Redirect to="/shop/products/all/1" />;
+    }
+  };
+
   render() {
     // https://tylermcginnis.com/react-router-pass-props-to-components/
     return (
@@ -34,8 +55,12 @@ class ShopConnector extends Component {
           to="/shop/products/:category/1"
           exact={true}
         />
-
+        {/* Refactored of the below to select the component that should be displayed to the user and provides it with props from the data store and URL router. */}
         <Route
+          path={"/shop/:section?/:category?/:page?"}
+          render={routeProps => this.selectComponent(routeProps)}
+        />
+        {/* <Route
           path={"/shop/products/:category/:page"}
           // routeProps = array of objects from api for each category [{match, location, history}]
           render={routeProps => (
@@ -49,8 +74,15 @@ class ShopConnector extends Component {
           // CartDetails component receives props from the data store and the routing system
           render={routeProps => <CartDetails {...this.props} {...routeProps} />}
         />
-        {/* Redirect matches any other URLs and redirects them to the URL for the first page of the products, unfiltered by category. */}
-        <Redirect to="/shop/products/all/1" />
+        <Route
+          path="/shop/checkout"
+          render={routeProps => <Checkout {...this.props} {...routeProps} />}
+        />
+        <Route
+          path="/shop/thanks"
+          render={routeProps => <Thanks {...this.props} {...routeProps} />}
+        />
+        <Redirect to="/shop/products/all/1" /> */}
       </Switch>
     );
   }
@@ -59,11 +91,8 @@ class ShopConnector extends Component {
 const mapStateToProps = dataStore => ({ ...dataStore });
 
 const mapDispatchToProps = {
-  loadData,
-  addToCart,
-  updateCartQuantity,
-  removeFromCart,
-  clearCart,
+  ...ShopActions,
+  ...CartActions,
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(ShopConnector);
